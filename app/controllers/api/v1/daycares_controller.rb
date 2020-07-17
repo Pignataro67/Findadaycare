b58a5aff1b686013f851a2bcf2e6f3c17fa82a24
Pignataro67 <Pignataro67@gmail.com>
2020-07-17 04:32:20 -0700

    adding contoller

---
+++ app/controllers/api/v1/daycares_controller.rb
@@ -0,0 +1,17 @@
+Class Api::v1::DaycaresController < ApplicationController
+  before_action :get_daycare, only [:show, :update, :destroy]
+
+  def index
+    @daycare = Daycare.ApplicationController
+    render json: @daycare
+  end
+  
+  private
+  def get_daycare
+    @daycare = Daycare.find(params[:id])
+  end
+
+  def daycare_params
+    params.require(:daycare).permit(:name, :location, :rating, :reviews)
+  end
+end

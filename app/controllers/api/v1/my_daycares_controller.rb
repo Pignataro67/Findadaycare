b7936aa139999fd8c40361209e7145e4d6f86195
Pignataro67 <Pignataro67@gmail.com>
2020-07-18 04:24:45 -0700

    adding private actions and params

--- app/controllers/api/v1/my_daycares_controller.rb
+++ app/controllers/api/v1/my_daycares_controller.rb
@@ -10,4 +10,17 @@ class Api::V1::MyDaycaresController < ApplicationController
   def show
     render json: @my_daycare
   end
+
+  private
+  def get_my_daycare
+    @my_daycare = MyDaycare.find(params[:id])
+  end
+
+  def get_user
+    @user = User.find(params[:user_id])
+  end
+
+  def my_daycare_params
+    params.require(:my_daycare).permit(:user_id, :daycare_id, :notes, :schedule_visit, :favorite)
+  end
 end

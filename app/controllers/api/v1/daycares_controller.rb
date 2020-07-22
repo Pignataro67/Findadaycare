c60780e8aef9a95da924a7eb68bbfe2ceb8eaefa
Pignataro67 <Pignataro67@gmail.com>
2020-07-22 05:33:02 -0700

    create action in cont.

--- app/controllers/api/v1/daycares_controller.rb
+++ app/controllers/api/v1/daycares_controller.rb
@@ -7,6 +7,29 @@ Class Api::v1::DaycaresController < ApplicationController
     render json: @daycares_json	        
   end
   
+  def create 
+    daycare = Daycare.find_by(yelp_id: params[:daycare][:yelp_id])
+    if !!daycare
+      if !!current_user.daycares.find_by(yelp_id: params[:daycare][:yelp_id])
+            
+      render json: {error: "You have already saved this daycare to your list"}
+        else
+          @mydaycare = MyDaycare.create(user_id: current_user.id, daycare_id: daycare.id)
+          my_daycare_json = MyDaycareSerializer.new(@my_daycare).serialized_json
+          render json: my_daycare_json
+      end
+    else
+      daycare = Daycare.new(daycare_params)
+      if daycare.save
+        @mydaycare = MyDaycare.create(user_id: current_user.id, daycare_id: daycare.id)
+        my_daycare_json = MyDaycareSerializer.new(@my_daycare).serialized_json
+        render json: my_daycare_json
+      else
+        render json: daycare.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
   private
   def get_daycare
     @daycare = Daycare.find(params[:id])

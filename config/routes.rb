d56dabfe7b2cce3b482d663a014f412c1f85dad0
Pignataro67 <Pignataro67@gmail.com>
2020-07-24 04:42:26 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    namespace :api do
+    namespace :v1 do
       resources :users do
         resources :my_daycares 
       end
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   end
   post '/api/v1/login', to: "api/v1/sessions#create"
   get '/api/v1/get_current_user', to: "api/v1/sessions#get_current_user"
+  delete '/api/v1/destroy', to: "api/v1/sessions#destroy"
+  post '/api/v1/signup', to: "api/v1/users#create"
 end
